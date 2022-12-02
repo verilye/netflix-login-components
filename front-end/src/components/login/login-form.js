3d1e67f24029fb68f22beda7255fba8c2a5e34cf
verilye <connor.j.logan@outlook.com>
2022-12-03 06:42:19 +1000

    Adjusted login form look

--- front-end/src/components/login/login-form.js
+++ front-end/src/components/login/login-form.js
@@ -8,35 +8,41 @@ export default class LoginForm extends React.Component{
     render(){
         return(
             <Box
-                mt="10%"
-                ml="37.5%"
-                mr="37.5%"
-                
+                mt="4%"
+                ml="38%"
+                mr="38%"
+                pl="3.5%"
                 sx={{
-                    backgroundColor:"rgba(0, 0, 0, .75)"
-                    
-                    }}
+                    textAlign:"left",
+                    backgroundColor:"rgba(0, 0, 0, .65)",
+                }}
             >   
-                <Box
-                    p="20%"
-                > 
                     <Box 
                         sx={{
-
-                            paddingTop:"10px",
-                            paddingBottom:"10px",
-                            fontSize:"25px",
-                            fontWeight:"bold"    
+                            paddingTop:"15%",
+                            paddingBottom:"5%",
+                            fontSize:"30px",
+                            fontWeight:"bold",
+                            fontColor:"white" 
                         }}
                     >
-                        Sign In</Box>  
+                        Sign In
+                    </Box>  
                     <FormControl>
                         <TextField
-                            fullWidth
+                            
                             id="email" 
                             label="Email address" 
                             variant="filled"
-                            sx={{input:{color:'black', backgroundColor:'white' }}}>
+                            sx={{
+                                input:{
+                                    color:'black', 
+                                    backgroundColor:'white',
+                                    width:'300px',
+                                    marginBottom:'20px',
+                                    borderRadius:'5px',
+                                }
+                                }}>
 
                         </TextField>
                         <TextField
@@ -44,12 +50,31 @@ export default class LoginForm extends React.Component{
                             id="password" 
                             label="Password" 
                             variant="filled"
-                            sx={{input:{color:'black', backgroundColor:'white'}}}>
+                            sx={{
+                                input:{
+                                    color:'black', 
+                                    backgroundColor:'white',
+                                    borderRadius:'5px',
+                                    marginBottom:'15%',
+                                    }
+                                }}>
                             
                         </TextField>
-                        <Button>The One Piece is REAL</Button>
+                        <Button 
+                            variant='contained' 
+                            style={{
+                                fontSize:"medium",
+                                backgroundColor:"#E50914",
+                                textTransform: "none",
+                                padding:"10px",
+                                fontWeight:"bold",
+                                marginBottom:"60%",
+                            }}
+                            href="/login"
+                        >     
+                            Sign In
+                        </Button>
                     </FormControl>
-                </Box>
             </Box>
         )
     }
