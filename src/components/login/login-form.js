24fcd79178bf491a9dcc5d9c7b2962138726582d
verilye <connor.j.logan@outlook.com>
2023-03-13 18:45:19 +1000

    update

---
+++ src/components/login/login-form.js
@@ -0,0 +1,81 @@
+import { FormControl, TextField, Button, Box } from "@mui/material"
+import React from "react"
+
+export default class LoginForm extends React.Component{
+
+    render(){
+        return(
+            <Box
+                mt="4%"
+                ml="38%"
+                mr="38%"
+                pl="3.5%"
+                sx={{
+                    textAlign:"left",
+                    backgroundColor:"rgba(0, 0, 0, .65)",
+                }}
+            >   
+                    <Box 
+                        sx={{
+                            paddingTop:"15%",
+                            paddingBottom:"5%",
+                            fontSize:"30px",
+                            fontWeight:"bold",
+                            fontColor:"white" 
+                        }}
+                    >
+                        Sign In
+                    </Box>  
+                    <FormControl>
+                        <TextField
+                            
+                            id="email" 
+                            label="Email address" 
+                            variant="filled"
+                            sx={{
+                                input:{
+                                    color:'black', 
+                                    backgroundColor:'white',
+                                    width:'300px',
+                                    marginBottom:'20px',
+                                    borderRadius:'5px',
+                                }
+                                }}>
+
+                        </TextField>
+                        <TextField
+                            fullWidth
+                            id="password" 
+                            label="Password" 
+                            variant="filled"
+                            sx={{
+                                input:{
+                                    color:'black', 
+                                    backgroundColor:'white',
+                                    borderRadius:'5px',
+                                    marginBottom:'15%',
+                                    }
+                                }}>
+                            
+                        </TextField>
+                        <Button 
+                            variant='contained' 
+                            style={{
+                                fontSize:"medium",
+                                backgroundColor:"#E50914",
+                                textTransform: "none",
+                                padding:"10px",
+                                fontWeight:"bold",
+                                marginBottom:"60%",
+                            }}
+                            href="/login"
+                        >     
+                            Sign In
+                        </Button>
+                    </FormControl>
+            </Box>
+        )
+    }
+
+
+}
---
+++ src/components/splash/navbar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Logo from '../../assets/logo.png';
+import Button from '@mui/material/Button';
+import { Box } from "@mui/system";
+
+
+export default class Navbar extends React.Component{
+
+    render(){
+        return(
+
+            <Box
+                component="span"
+                display="flex"
+                justifyContent="space-between">
+                        <a href="/">
+                            <img 
+                                alt={"alt"}
+                                src={Logo}
+                                sx={{ flexGrow: 1 }}
+                                style={{
+                                    maxHeight:"70px",
+                                    margin:'20px',
+                                    marginLeft:"40px"
+                                }}
+                            />
+                        </a>
+                    
+                        <Button 
+                            variant='contained' 
+                            style={{
+                                fontSize:"medium",
+                                backgroundColor:"#E50914",
+                                maxWidth: '120px', 
+                                maxHeight: '40px',
+                                textTransform: "none",
+                                margin:'30px',
+                                marginRight:"60px"
+                            }}
+                            href="/login"
+                        >     
+                            Sign In
+                        </Button>
+                        
+            </Box>
+            
+        );
+    }
+   
+}
